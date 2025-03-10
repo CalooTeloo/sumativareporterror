28cfbcbee05673087b3f71a8e9b265da0ec5d974
calichitello0 <calichitello0@gmail.com>
2025-03-10 05:03:22 +0000

    segundo commit

---
+++ src/app/Views/add-report/add-report.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-report',
+  imports: [],
+  templateUrl: './add-report.component.html',
+  styleUrl: './add-report.component.css'
+})
+export class AddReportComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,22 +4,27 @@ export const routes: Routes = [
 
     {
         path: '',
-        loadComponent: () => import('./Views/report-list/report-list.component').then (m => m.ReportListComponent)
+        loadComponent: () => import('./Views/report-list/report-list.component').then(m => m.ReportListComponent)
     },
 
     {
-        path:'report/:id',
-        loadComponent: () => import('./Views/report-detail/report-detail.component').then (m => m.ReportDetailComponent)
+        path: 'add-report',
+        loadComponent: () => import('./Views/add-report/add-report.component').then(m => m.AddReportComponent)
     },
 
     {
-        path:'not-found/:id',
-        loadComponent: () => import('./Views/not-found/not-found.component').then (m => m.NotFoundComponent)
+        path: 'report/:id',
+        loadComponent: () => import('./Views/report-detail/report-detail.component').then(m => m.ReportDetailComponent)
+    },
+
+    {
+        path: 'not-found/:id',
+        loadComponent: () => import('./Views/not-found/not-found.component').then(m => m.NotFoundComponent)
     },
 
     {
         path: '**',
-        redirectTo : '',
+        redirectTo: '',
         pathMatch: 'full'
     }
 
